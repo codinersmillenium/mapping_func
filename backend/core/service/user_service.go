6c0b42b2a8a10c6f148278a41b66209d9c4e66ef
codinersmillenium <75132712+codinersmillenium@users.noreply.github.com>
2026-02-07 23:02:06 +0700

    fix: wrong file

--- backend/core/service/user_service.go
+++ backend/core/service/user_service.go
@@ -1,41 +1,30 @@
 package service
 
 import (
-	"fmt"
+	"mapping_func/core/adapter"
 	"mapping_func/core/entity"
 	"mapping_func/core/repository"
-	"strings"
 )
 
 type UserService struct {
 	repo   repository.UserRepository
-	cities map[string]string
+	parser *adapter.ParserAdapter
 }
 
-func NewUserService(r repository.UserRepository, c map[string]string) *UserService {
-	return &UserService{r, c}
+func NewUserService(r repository.UserRepository, cities map[string]string) *UserService {
+	parser := adapter.NewParserAdapter(cities)
+	return &UserService{
+		repo:   r,
+		parser: parser,
+	}
 }
 
 func (s *UserService) Create(input string) error {
-	parts := strings.Split(input, " ")
-
-	if len(parts) < 3 {
-		return nil
-	}
-
-	age := parts[len(parts)-2]
-	city := strings.ToUpper(parts[len(parts)-1])
-	name := strings.ToUpper(strings.Join(parts[:len(parts)-2], " "))
-	fmt.Println(age)
-	if p, ok := s.cities[city]; ok {
-		city = city + " " + p
-	}
-
+	name, age, city := s.parser.Parse(input)
 	u := &entity.User{
 		Name: name,
 		Age:  age,
 		City: city,
 	}
-
 	return s.repo.Insert(u)
 }
